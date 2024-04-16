41884a003b7e2c63c1de4b5c76dc3f7138d933a0
GUBONWOO <zcwxzsx@gmail.com>
2024-04-16 18:05:21 +0900

    toggle and delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoItemCreator from './components/TodoItemCreator';
+import { useRecoilValue } from 'recoil';
+import { todoListState } from './todoAtoms';
+import TodoItem from './components/TodoItem';
 
 function App() {
+  const todoList = useRecoilValue(todoListState);
+  console.log(todoList);
   return (
     <div className='App'>
       <TodoItemCreator />
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
     </div>
   );
 }
