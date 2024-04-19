f5b0238afe4fdd8591eebe6101853cb7c45c882c
GUBONWOO <zcwxzsx@gmail.com>
2024-04-19 17:07:39 +0900

    data query

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { filteredTodoListState, todoListState } from './todoAtoms';
 import TodoItem from './components/TodoItem';
 import TodoListFiltered from './components/TodoListFiltered';
 import TodoListStats from './components/TodoListStats';
+import { currentUserNameQuery } from './userAtoms';
+import React from 'react';
 
 function App() {
   const todoList = useRecoilValue(todoListState);
@@ -12,6 +14,9 @@ function App() {
   console.log(todoList);
   return (
     <div className='App'>
+      <React.Suspense fallback={<div>...Loading</div>}>
+        <CurrentUserInfo />
+      </React.Suspense>
       <TodoListStats />
       <TodoListFiltered />
       <TodoItemCreator />
@@ -23,3 +28,8 @@ function App() {
 }
 
 export default App;
+
+function CurrentUserInfo() {
+  const userName = useRecoilValue(currentUserNameQuery);
+  return <div>{userName}</div>;
+}
